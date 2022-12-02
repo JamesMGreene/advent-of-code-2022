2c8a1d820011d645a861c20d8bfa4f3f3ab79c24
James M. Greene <JamesMGreene@github.com>
2022-12-02 00:48:02 -0600

    Add day01 combined solution

---
+++ day01/combined/main.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S deno run --allow-read
+
+import { getInputSectionStream } from '../../helpers/file.ts'
+import { sortAscending, sum, sumLines } from '../../helpers/array.ts'
+
+// Prepare the processing functions
+const highestValues = [0, 0, 0]
+
+function maybeUpdateHighestValues(currentSum:number) {
+  // If the current sum is higher than the previous highest value,
+  // update the highest value
+  if (currentSum > highestValues[0]) {
+    highestValues[0] = currentSum
+
+    // Follow-up sorting
+    sortAscending(highestValues)
+  }
+}
+
+// Get a readable stream from the input file doesn't have to be fully loaded into memory
+const sectionReader = await getInputSectionStream()
+
+// Assess each group of numbers
+for await (const lines of sectionReader) {
+  // ⚠️ If there are a lot of lines, this could get slow as we already read them once creating the SectionReader
+  const currentSum = sumLines(lines)
+  maybeUpdateHighestValues(currentSum)
+}
+
+//console.log('Highest values: ' + JSON.stringify(highestValues))
+console.log('[pt1] Highest value: ' + highestValues[2])
+console.log('[pt2] Highest 3 values, summed: ' + sum(highestValues))
