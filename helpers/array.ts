f8eb3ae3e6542d9faece376f073e19d1533c30ec
James M. Greene <JamesMGreene@github.com>
2022-12-03 01:24:25 -0600

    Remove helper functions that lodash covers

--- helpers/array.ts
+++ helpers/array.ts
@@ -1,11 +1,3 @@
-export function sumLines(array:string[]): number {
-  return array.reduce((acc:number, current:string) => acc + Number(current), 0)
-}
-
-export function sum(array:number[]): number {
-  return array.reduce((acc:number, current:number) => acc + current, 0)
-}
-
 export function sortAscending(array:number[]): void {
   array.sort((a, b) => a - b)
 }
