660cd5b09c29f63d8dfcae50afafd04efc257227
James M. Greene <JamesMGreene@github.com>
2022-12-03 23:41:47 -0600

    Revise to not use arrays for ranges to prevent memory explosions

--- day04/main.ts
+++ day04/main.ts
@@ -2,14 +2,52 @@
 
 import { getInputRowStream } from '../helpers/file.ts'
 import { getInputFileName } from '../helpers/args.ts'
-import { _ } from '../helpers/lodash.ts'
+
+interface IRange {
+  min: number,
+  max: number
+}
+
+enum OverlapType {
+  None = 'none',
+  Partial = 'partial',
+  Full = 'full'
+}
 
 //
 // Processing functions
 //
-function getRange(input:string) : number[] {
+function getRange(input:string) : IRange {
   const [min, max] = input.split('-').map(Number)
-  return _.range(min, max + 1)
+  return { min, max }
+}
+
+function getOverlapType(range1:IRange, range2:IRange) : OverlapType {
+  // Left overlap
+  if (range1.min <= range2.min) {
+    if (range1.max >= range2.max) {
+      return OverlapType.Full
+    } else if (range1.max >= range2.min) {
+      return OverlapType.Partial
+    }
+  }
+  // Right overlap
+  else if (range1.min <= range2.max && range1.max >= range2.max) {
+    return OverlapType.Partial
+  }
+  return OverlapType.None
+}
+
+function getBidirectionalOverlapType(range1:IRange, range2:IRange) : OverlapType {
+  const overlap1 = getOverlapType(range1, range2)
+  //console.debug('range1', range1, 'range2', range2)
+  //console.debug('overlap1', overlap1)
+  if (overlap1 !== OverlapType.Full) {
+    const overlap2 = getOverlapType(range2, range1)
+    //console.debug('overlap2', overlap2)
+    return overlap2
+  }
+  return overlap1
 }
 
 // Get a readable stream from the input file doesn't have to be fully loaded into memory
@@ -22,11 +60,12 @@ let partialOverlaps = 0
 for await (const [firstAssignment, secondAssignment] of rowRangeReader) {
   const firstRange = getRange(firstAssignment)
   const secondRange = getRange(secondAssignment)
-  const overlap = _.intersection(firstRange, secondRange)
-  if (overlap.length === firstRange.length || overlap.length === secondRange.length) {
+
+  const overlapType = getBidirectionalOverlapType(firstRange, secondRange)
+  if (overlapType === 'full') {
     fullOverlaps++
-  }
-  if (overlap.length > 0) {
+    partialOverlaps++
+  } else if (overlapType === 'partial') {
     partialOverlaps++
   }
 }
