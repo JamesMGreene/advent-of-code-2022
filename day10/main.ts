9207fb0d855726224c6e6d6b0e902ef13d57a720
James M. Greene <JamesMGreene@github.com>
2022-12-10 00:27:31 -0600

    Some visual improvements

--- day10/main.ts
+++ day10/main.ts
@@ -1,6 +1,12 @@
 import { getInputRowStream } from '../helpers/file.ts'
 import { getInputFileName } from '../helpers/args.ts'
 
+// Constants
+const SCREEN_WIDTH = 40
+const SCREEN_HEIGHT = 6
+const LIT_PIXEL = '🟩' // '#'
+const DARK_PIXEL = '⬛' // '.'
+
 //
 // Prepare the processing functions
 //
@@ -21,27 +27,33 @@ function calculatePart1() {
   return sum
 }
 
+function displayPart2() {
+  return crtScreen
+    .map(row => row.join(''))
+    .join('\n')
+}
+
 // Get a readable stream from the input file doesn't have to be fully loaded into memory
 const instructionReader = await getInputRowStream(getInputFileName())
 
 let signalRegister = 1
 const cycleStrengthRecords:number[] = []
 // Screen is 40 pixels wide by 6 pixels tall
-const crtScreen:string[][] = Array(6).fill(0).map(_ => Array(40).fill('.'))
+const crtScreen:string[][] = Array(SCREEN_HEIGHT).fill(0).map(_ => Array(SCREEN_WIDTH).fill(DARK_PIXEL))
 
 for await (const [instruction, value] of instructionReader) {
   let cycleIndex = cycleStrengthRecords.length
   let cycleNumber = cycleIndex + 1
-  let crtRow = Math.floor(cycleIndex / 40)
-  let crtCol = cycleIndex % 40
+  let crtRow = Math.floor(cycleIndex / SCREEN_WIDTH)
+  let crtCol = cycleIndex % SCREEN_WIDTH
 
-  console.debug({ cycleIndex, cycleNumber, crtRow, crtCol, signalRegister })
+  //console.debug({ cycleIndex, cycleNumber, crtRow, crtCol, signalRegister })
   //console.debug(`Signal register starting cycle #${cycleNumber}: ${signalRegister}`)
 
   if (instruction === 'noop') {
     // Spend 1 cycle doing nothing
     if (crtCol >= signalRegister - 1 && crtCol <= signalRegister + 1) {
-      crtScreen[crtRow][crtCol] = '#'
+      crtScreen[crtRow][crtCol] = LIT_PIXEL
     }
     cycleStrengthRecords.push(signalRegister * cycleNumber)
   }
@@ -49,7 +61,7 @@ for await (const [instruction, value] of instructionReader) {
     // Spend 2 cycles adding the value to the signal register
     const valueNum = Number(value)
     if (crtCol >= signalRegister - 1 && crtCol <= signalRegister + 1) {
-      crtScreen[crtRow][crtCol] = '#'
+      crtScreen[crtRow][crtCol] = LIT_PIXEL
     }
     cycleStrengthRecords.push(signalRegister * cycleNumber)
 
@@ -59,11 +71,11 @@ for await (const [instruction, value] of instructionReader) {
     crtRow = Math.floor(cycleIndex / 40)
     crtCol = cycleIndex % 40
 
-    console.debug({ cycleIndex, cycleNumber, crtRow, crtCol, signalRegister })
+    //console.debug({ cycleIndex, cycleNumber, crtRow, crtCol, signalRegister })
     //console.debug(`Signal register starting cycle #${cycleNumber}: ${signalRegister}`)
 
     if (crtCol >= signalRegister - 1 && crtCol <= signalRegister + 1) {
-      crtScreen[crtRow][crtCol] = '#'
+      crtScreen[crtRow][crtCol] = LIT_PIXEL
     }
     cycleStrengthRecords.push(signalRegister * cycleNumber)
 
@@ -73,4 +85,4 @@ for await (const [instruction, value] of instructionReader) {
 //console.debug(`Final register value after cycle #${cycleStrengthRecords.length + 1}: ${signalRegister}`)
 
 console.log('[pt1] Sum of stuff: ' + calculatePart1())
-console.log('[pt2] CRT screen output:\n' + crtScreen.map(row => row.join('')).join('\n'))
+console.log('[pt2] CRT screen output:\n' + displayPart2())
